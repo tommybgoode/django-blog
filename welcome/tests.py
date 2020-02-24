c14266021319083518b19e284f104d520ce27429
tommybgoode <thombla1@uk.ibm.com>
2020-02-24 21:32:24 +0000

    change manage.py permissions

--- welcome/tests.py
+++ welcome/tests.py
@@ -6,11 +6,6 @@ from django.test import TestCase
 
 # These basic tests are to be used as an example for running tests in S2I
 # and OpenShift when building an application image.
-class PageViewModelTest(TestCase):
-    def test_viewpage_model(self):
-        pageview = PageView.objects.create(hostname='localhost')
-        pagetest = PageView.objects.get(hostname='localhost')
-        self.assertEqual(pagetest.hostname, 'localhost')
 
 class PageViewTest(TestCase):
     def test_index(self):
